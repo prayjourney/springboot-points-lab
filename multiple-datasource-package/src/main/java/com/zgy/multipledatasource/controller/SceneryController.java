e714bd095c0c1370e61015eefbeee5c3bb53c7c7
prayjourney.io <2247359268@qq.com>
2021-08-02 22:51:41 +0800

    ⛳update

--- multiple-datasource-package/src/main/java/com/zgy/multipledatasource/controller/SceneryController.java
+++ multiple-datasource-package/src/main/java/com/zgy/multipledatasource/controller/SceneryController.java
@@ -39,7 +39,7 @@ public class SceneryController {
 
     @RequestMapping("name")
     public String getSceneryNameById(Integer id) {
-        log.info("getSceneryNameById" );
+        log.info("getSceneryNameById");
         return sceneryService.getSceneryNameById(id);
     }
 }
--- multiple-datasource-package/src/main/java/com/zgy/multipledatasource/pojo/Scenery.java
+++ multiple-datasource-package/src/main/java/com/zgy/multipledatasource/pojo/Scenery.java
@@ -1,7 +1,6 @@
 package com.zgy.multipledatasource.pojo;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
