a519d32901136abe87282f6d701539f73fdb3cd2
prayjourney.io <2247359268@qq.com>
2021-08-02 23:02:41 +0800

    📊使用easypoi完成对于excel的读取和写入

---
+++ excel-easypoi/src/main/java/com/zgy/learn/easypoi/EasyPoiApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.easypoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyPoiApplication.class, args);
+    }
+
+}
---
+++ excel-easypoi/src/test/java/com/zgy/learn/easypoi/EasyPoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.easypoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
