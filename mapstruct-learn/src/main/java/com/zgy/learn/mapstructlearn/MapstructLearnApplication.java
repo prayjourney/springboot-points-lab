534d49a05401a18d28611139062baf16e28f45ad
prayjourney.io <2247359268@qq.com>
2021-09-27 01:38:47 +0800

    mapstruct的学习使用

---
+++ mapstruct-learn/src/main/java/com/zgy/learn/mapstructlearn/MapstructLearnApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.mapstructlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructLearnApplication.class, args);
+    }
+
+}
---
+++ mapstruct-learn/src/test/java/com/zgy/learn/mapstructlearn/MapstructLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.mapstructlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
