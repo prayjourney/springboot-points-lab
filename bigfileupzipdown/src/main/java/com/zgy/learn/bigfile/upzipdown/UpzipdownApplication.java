1a32d1d106aabfe0ef6119bf727a73b6538718a6
prayjourney.io <2247359268@qq.com>
2021-09-27 01:45:31 +0800

    ⛳bigfile分片上传，压缩下载

---
+++ bigfileupzipdown/src/main/java/com/zgy/learn/bigfile/upzipdown/UpzipdownApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.bigfile.upzipdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpzipdownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpzipdownApplication.class, args);
+    }
+
+}
---
+++ bigfileupzipdown/src/test/java/com/zgy/learn/bigfile/upzipdown/UpzipdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.bigfile.upzipdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpzipdownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
