9d9638a872933068ce461d35f01bd583a1b291e3
prayjourney.io <2247359268@qq.com>
2021-09-28 01:16:42 +0800

    ⛳如何书写整洁干净的代码

---
+++ beautiful-clean-code/src/test/java/com/zgy/learn/beautifulcleancode/BeautifulCleanCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.beautifulcleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulCleanCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
