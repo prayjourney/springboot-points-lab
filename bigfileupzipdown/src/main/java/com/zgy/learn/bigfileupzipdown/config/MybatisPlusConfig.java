6ab5593624fb78fbabac919e4999055861ec4a3e
prayjourney.io <2247359268@qq.com>
2021-10-06 21:06:20 +0800

    配置

---
+++ bigfileupzipdown/src/main/java/com/zgy/learn/bigfileupzipdown/config/MybatisPlusConfig.java
@@ -0,0 +1,31 @@
+package com.zgy.learn.bigfileupzipdown.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author: pray-journey.io
+ * @description:
+ * @date: created in 2021/10/6
+ * @modified:
+ */
+@EnableTransactionManagement
+@MapperScan(basePackages = "com.zgy.learn.bigfileupzipdown.mapper")
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+}
