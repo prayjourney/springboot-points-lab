2e1f14175eaf1aed2b77759a98d239cdc06b77fd
prayjourney.io <2247359268@qq.com>
2021-10-06 21:33:54 +0800

    🎨service

---
+++ bigfileupzipdown/src/main/java/com/zgy/learn/bigfileupzipdown/service/FileRecordService.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.bigfileupzipdown.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: pray-journey.io
+ * @description:
+ * @date: created in 2021/10/6
+ * @modified:
+ */
+@Service
+public class FileRecordService {
+}
