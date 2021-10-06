98b8d1a0248305d5476736d7f585756f75890bb5
prayjourney.io <2247359268@qq.com>
2021-10-06 21:07:06 +0800

    🎗mapper

---
+++ bigfileupzipdown/src/main/java/com/zgy/learn/bigfileupzipdown/mapper/FileRecordMapper.java
@@ -0,0 +1,19 @@
+package com.zgy.learn.bigfileupzipdown.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+/**
+ * @author: pray-journey.io
+ * @description:
+ * @date: created in 2021/10/6
+ * @modified:
+ */
+@Mapper
+@Repository
+public interface FileRecordMapper {
+
+
+}
