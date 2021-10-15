91d1a7ecf2aee71a7421a9db43932eddb7e14427
linzhaoxi <2247359268@qq.com>
2021-10-15 15:55:53 +0800

    🍎update

--- hessian-server/src/main/java/com/zgy/learn/hessianserver/config/HessianServerConfig.java
+++ hessian-server/src/main/java/com/zgy/learn/hessianserver/config/HessianServerConfig.java
@@ -1,21 +1,25 @@
-package com.zgy.learn.hessianserver.component;
+package com.zgy.learn.hessianserver.config;
 
 import com.zgy.learn.hessianserver.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.caucho.HessianServiceExporter;
-import org.springframework.stereotype.Component;
 
 /**
- * @author renjiaxin
+ * @author zgy
  * @date 2021/10/15
  */
-@Component
-public class HessianServerComponent {
+@Configuration
+public class HessianServerConfig {
 
     @Autowired
     private HelloService helloService;
 
+    /**
+     * 1. HessianServiceExporter是由Spring.web框架提供的Hessian工具类,能够将bean转化为Hessian服务
+     * 2. @Bean(name = "/hessian")加斜杠方式会被spring暴露服务路径, 发布服务。
+     */
     @Bean(name = "/hessian")
     public HessianServiceExporter hessianServiceExporter() {
         // 使用Spring的HessianService做代理
@@ -24,4 +28,5 @@ public class HessianServerComponent {
         hessianServiceExporter.setServiceInterface(HelloService.class);
         return hessianServiceExporter;
     }
+
 }
