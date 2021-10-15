8aa8df289675f7b0adb98667e4d58b89a6ea0185
linzhaoxi <2247359268@qq.com>
2021-10-15 15:47:48 +0800

    🍚初始化hessian的client

---
+++ hessian-client/src/main/java/com/zgy/learn/hessianclient/HessianClientApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.hessianclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HessianClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HessianClientApplication.class, args);
+    }
+
+}
