e89b6ba6da01ec96cb1bb6bfc7658fff91b95adf
prayjourney.io <2247359268@qq.com>
2021-10-18 01:23:40 +0800

    🧵update，都要添加@EnableDubbo

--- dubbo-shop-consumer/src/main/java/com/zgy/learn/dubboshopconsumer/DubboShopConsumerApplication.java
+++ dubbo-shop-consumer/src/main/java/com/zgy/learn/dubboshopconsumer/DubboShopConsumerApplication.java
@@ -1,8 +1,10 @@
 package com.zgy.learn.dubboshopconsumer;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
 @SpringBootApplication
 public class DubboShopConsumerApplication {
 
