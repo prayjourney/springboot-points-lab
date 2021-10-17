17328f7827c1f39d506d072f2f05d8ac118db2ab
prayjourney.io <2247359268@qq.com>
2021-10-18 00:29:51 +0800

    🎈消费者初始化

---
+++ dubbo-shop-consumer/src/main/java/com/zgy/learn/dubboshopconsumer/DubboShopConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.dubboshopconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboShopConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboShopConsumerApplication.class, args);
+    }
+
+}
---
+++ dubbo-shop-consumer/src/test/java/com/zgy/learn/dubboshopconsumer/DubboShopConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.dubboshopconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboShopConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
