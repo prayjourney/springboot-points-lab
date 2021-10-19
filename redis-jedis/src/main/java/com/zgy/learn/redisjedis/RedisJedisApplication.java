9530ee363e9ac59d04a2032c1aec04f86dee31fe
linzhaoxi <2247359268@qq.com>
2021-10-19 12:06:07 +0800

    redis使用jedis客户端

---
+++ redis-jedis/src/main/java/com/zgy/learn/redisjedis/RedisJedisApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.redisjedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisJedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisJedisApplication.class, args);
+    }
+
+}
---
+++ redis-jedis/src/test/java/com/zgy/learn/redisjedis/RedisJedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.redisjedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisJedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
