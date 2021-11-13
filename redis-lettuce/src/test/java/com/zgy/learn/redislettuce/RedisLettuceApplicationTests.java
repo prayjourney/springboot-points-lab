9082b6aa897c1a3966032083ac564683375cb504
prayjourney.io <2247359268@qq.com>
2021-11-13 16:52:02 +0800

    🎈redis+lettuce整合

---
+++ redis-lettuce/src/test/java/com/zgy/learn/redislettuce/RedisLettuceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.redislettuce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLettuceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
