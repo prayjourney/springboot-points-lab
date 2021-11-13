da7a9a145f9a309aa66efff919259da7c9ef4872
prayjourney.io <2247359268@qq.com>
2021-11-13 18:40:49 +0800

    🎈pojo

---
+++ redis-lettuce/src/main/java/com/zgy/learn/redislettuce/pojo/User.java
@@ -0,0 +1,23 @@
+package com.zgy.learn.redislettuce.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @author: pray-journey.io
+ * @date: created in 2021/11/13
+ * @description:
+ */
+@Getter
+@Setter
+@Accessors(chain = true)
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    private String address;
+
+}
