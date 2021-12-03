29740396e3bb62a26e1c0c0f65fee08804879280
linzhaoxi <2247359268@qq.com>
2021-12-03 11:24:43 +0800

    🍕init,处理xml,

---
+++ xml-deal/src/main/java/com/zgy/learn/xmldeal/XmlDealApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.xmldeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlDealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlDealApplication.class, args);
+    }
+
+}
---
+++ xml-deal/src/test/java/com/zgy/learn/xmldeal/XmlDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.xmldeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlDealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
