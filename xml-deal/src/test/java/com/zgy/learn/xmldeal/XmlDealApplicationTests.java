a579d6acb78d1c11993807c70fd008a599a67e34
linzhaoxi <2247359268@qq.com>
2021-12-03 14:06:36 +0800

    🍕测试

--- xml-deal/src/test/java/com/zgy/learn/xmldeal/XmlDealApplicationTests.java
+++ xml-deal/src/test/java/com/zgy/learn/xmldeal/XmlDealApplicationTests.java
@@ -1,13 +1,34 @@
 package com.zgy.learn.xmldeal;
 
+import com.zgy.learn.xmldeal.service.XmlService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootTest
 class XmlDealApplicationTests {
 
+    @Resource
+    private XmlService xmlService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testXmlstrMap() {
+        Map<String, Object> stringObjectMap = xmlService.parseXml();
+        Set<String> strings = stringObjectMap.keySet();
+        Iterator<String> iterator = strings.iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            Object value = stringObjectMap.get(key);
+            System.out.println("key: " + key + ", value: " + value);
+        }
     }
 
 }
