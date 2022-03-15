95ea42f8b619b4b086251b5d892dcda65e275b97
prayjourney.io <2247359268@qq.com>
2022-03-16 01:45:15 +0800

    转换工具类

---
+++ boot-gson/src/main/java/com/zgy/learn/bootgson/util/Converter.java
@@ -0,0 +1,45 @@
+package com.zgy.learn.bootgson.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: pray-journey.io
+ * @description:
+ * @date: created in 2022/3/16
+ * @modified:
+ */
+@Component
+public class Converter {
+    @Autowired
+    @Qualifier(value = "gsonsettype")
+    private Gson gson;
+
+    /**
+     * jsonstr转JsonObject
+     *
+     * @param jsonStr
+     * @return
+     */
+    public JsonObject str2Obj(String jsonStr) {
+        JsonObject jsonObject = gson.fromJson(jsonStr, JsonObject.class);
+        return jsonObject;
+    }
+
+    /**
+     * jsonstr转JsonArray
+     *
+     * @param jsonStr
+     * @return
+     */
+    public JsonArray str2Array(String jsonStr) {
+        JsonArray jsonArray = gson.fromJson(jsonStr, JsonArray.class);
+        return jsonArray;
+    }
+
+
+}
