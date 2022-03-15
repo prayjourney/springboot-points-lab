fa5e113a7df6dc72bd6b67e6a41e987a9e405c5d
prayjourney.io <2247359268@qq.com>
2022-03-16 00:29:00 +0800

    创建一个JsonArray

--- boot-gson/src/main/java/com/zgy/learn/bootgson/GsonController.java
+++ boot-gson/src/main/java/com/zgy/learn/bootgson/GsonController.java
@@ -1,5 +1,6 @@
 package com.zgy.learn.bootgson;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,12 @@ public class GsonController {
 
     /**
      * 创建一个JsonObject
+     * JsonObject里面只能存放字符串, 数字, 布尔类型, 嵌套JsonObject或者是JsonArray
      *
      * @return
      */
     @RequestMapping("create/jsonobject")
-    public String createGson() {
+    public String createJsonObject() {
         JsonObject jsonObject = new JsonObject();
         // 添加属性
         jsonObject.addProperty("country", "ZHN");
@@ -37,4 +39,29 @@ public class GsonController {
         return jsonObject.toString();
 
     }
+
+    /**
+     * 创建一个JsonArray
+     * 创建一个JsonArray里面只能存放字符串, 数字, 布尔类型, 嵌套JsonObject或者是JsonArray
+     *
+     * @return
+     */
+    @RequestMapping("create/jsonarrray")
+    public String createJsonArray() {
+        JsonArray jsonArray = new JsonArray();
+        jsonArray.add("张三");
+        jsonArray.add("李四");
+        jsonArray.add("王二麻子");
+        System.out.println(jsonArray);
+        // 添加对象
+        JsonObject object = new JsonObject();
+        object.addProperty("name", "韩梅梅");
+        object.addProperty("age", 28);
+        jsonArray.add(object);
+        System.out.println(jsonArray);
+
+        // 打印JsonArray的字符串
+        return jsonArray.toString();
+    }
+
 }
