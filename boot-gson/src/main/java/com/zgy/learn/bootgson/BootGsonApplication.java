25b605d6a53b291f8ae21a0618aa04d694229bfe
prayjourney.io <2247359268@qq.com>
2022-03-15 23:51:50 +0800

    springboot整合gson

---
+++ boot-gson/src/main/java/com/zgy/learn/bootgson/BootGsonApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.bootgson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootGsonApplication.class, args);
+    }
+
+}
---
+++ boot-gson/src/test/java/com/zgy/learn/bootgson/BootGsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.bootgson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
