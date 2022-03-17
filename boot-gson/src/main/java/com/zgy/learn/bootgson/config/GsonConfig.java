32e7c943ff89804d336dbf605eb2c85369ec4396
linzhaoxi <2247359268@qq.com>
2022-03-17 12:06:29 +0800

    🌻配置

--- boot-gson/src/main/java/com/zgy/learn/bootgson/config/GsonConfig.java
+++ boot-gson/src/main/java/com/zgy/learn/bootgson/config/GsonConfig.java
@@ -25,7 +25,24 @@ public class GsonConfig {
 
     @Bean(name = "gsonsettype")
     public Gson gsonsettype() {
-        return new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+        return new GsonBuilder()
+                .serializeNulls() // 默认情况下如果某一个属性为null, 那么此属性不会参与序列化和反序列化的过程, 加上此属性后会参与序列化和反序列化的过程
+                .setPrettyPrinting()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss").create();
     }
+    // setPrettyPrinting(): 格式化json字符串的输出, 默认情况下是输出一行, 经过这个属性设置后会格式化输出, 即有缩进的输出
+    // {"p1":{"id":"18239fg1","name":"张璇","age":22,"gender":1,"address":"北京","birthday":"2022-03-17 11:59:19"}}
+    /**
+    {
+        "p1": {
+        "id": "18239fg1",
+                "name": "张璇",
+                "age": 22,
+                "gender": 1,
+                "address": "北京",
+                "birthday": "2022-03-17 11:59:19"
+        }
+    }
+    */
 
 }
