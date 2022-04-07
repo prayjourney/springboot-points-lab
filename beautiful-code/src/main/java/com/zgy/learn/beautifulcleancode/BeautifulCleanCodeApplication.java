0a666175944613b065948b5f8866fd4557f0ba2a
prayjourney.io <2247359268@qq.com>
2022-04-08 01:11:19 +0800

    🚀rename

---
+++ beautiful-code/src/main/java/com/zgy/learn/beautifulcleancode/BeautifulCleanCodeApplication.java
@@ -0,0 +1,14 @@
+package com.zgy.learn.beautifulcleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulCleanCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautifulCleanCodeApplication.class, args);
+
+    }
+
+}
--- beautiful-code/src/main/java/com/zgy/learn/beautifulcleancode/pojo/People.java
+++ beautiful-code/src/main/java/com/zgy/learn/beautifulcleancode/pojo/People.java
@@ -1,6 +1,7 @@
 package com.zgy.learn.beautifulcleancode.pojo;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -19,6 +20,7 @@ import java.util.Date;
 @Setter
 @ToString
 @Accessors(chain = true)
+@NoArgsConstructor
 @RequiredArgsConstructor // 生成指定字段的构造器， @NonNull / final都可使其起作用
 public class People {
     @NonNull
