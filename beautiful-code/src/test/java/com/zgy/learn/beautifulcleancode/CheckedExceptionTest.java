762741c202328d7862663898c7d0b74342db9326
prayjourney.io <2247359268@qq.com>
2022-04-08 01:18:41 +0800

    异常测试

---
+++ beautiful-code/src/test/java/com/zgy/learn/beautifulcleancode/CheckedExceptionTest.java
@@ -0,0 +1,39 @@
+package com.zgy.learn.beautifulcleancode;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class CheckedExceptionTest {
+    public static void main(String[] args) throws Exception {
+        String str1 = "d://临江仙·柳絮.txt";
+        String str2 = "d://hello.txt";
+        System.out.println(readFile01(str1));
+        System.out.println(readFile01(str2));
+
+        System.out.println(readFile02(""));
+
+    }
+
+    public static String readFile01(String str) {
+        // try-catch-finally处理异常
+        try {
+            File file = new File(str);
+            return FileUtils.readFileToString(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally一定会执行!");
+        }
+        return "error!";
+    }
+
+    public static String readFile02(String str) throws Exception {
+        // 抛出异常
+        if (str.length() == 0) {
+            throw new Exception("error");
+        }
+        return "error!";
+    }
+
+}
---
+++ beautiful-code/src/test/java/com/zgy/learn/beautifulcleancode/ExceptionAndReturnTest.java
@@ -0,0 +1,60 @@
+package com.zgy.learn.beautifulcleancode;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionAndReturnTest {
+    public static void main(String[] args) throws IOException {
+        // System.out.println(readFile01(""));
+        // System.out.println(readFile02(""));
+        System.out.println(readFile03(""));
+    }
+
+    public static String readFile01(String str) {
+        // try-catch-finally处理异常
+        try {
+            File file = new File(str);
+            String content = FileUtils.readFileToString(file);
+            return content;
+        } catch (Exception e) {
+            System.out.println("readFile01发生错误!");
+            e.printStackTrace();
+            return "return: readFile01: catch之中的返回值!";     // A
+        } finally {
+            System.out.println("finally一定会执行!readFile01");
+            return "return: readFile01 finally~~~";           // B
+        }
+        // return "return: readFile01!";                      // C
+    }
+
+    public static String readFile02(String str) {
+        // try-catch处理异常
+        try {
+            File file = new File(str);
+            String content = FileUtils.readFileToString(file);
+            return content;
+            // return FileUtils.readFileToString(file);
+        } catch (Exception e) {
+            System.out.println("readFile02发生错误!");
+            e.printStackTrace();
+            // return "return: readFile02: catch之中的返回值!";
+        }
+        return "return: readFile02!";
+    }
+
+    public static String readFile03(String str) throws IOException {
+        // try-finally处理异常
+        try {
+            File file = new File(str);
+            String content = FileUtils.readFileToString(file);
+            return content;
+        } finally {
+            System.out.println("finally一定会执行!readFile03");
+            return "return: readFile03: finally之中的返回值!";
+        }
+        // Unreachable statement, 下面在无法添加返回String了
+        // return content;
+    }
+}
