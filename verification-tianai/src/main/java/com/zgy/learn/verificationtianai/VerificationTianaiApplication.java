4613bcb9360359b4bb0e0ef5c4ac237e657847bf
linzhaoxi <2247359268@qq.com>
2022-04-22 16:39:48 +0800

    🍕init

---
+++ verification-tianai/src/main/java/com/zgy/learn/verificationtianai/VerificationTianaiApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.verificationtianai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationTianaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerificationTianaiApplication.class, args);
+    }
+
+}
