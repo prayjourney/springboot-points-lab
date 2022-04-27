aa7bc1dc051a0357ffc25e4e1989c4b16e3b1adf
prayjourney.io <2247359268@qq.com>
2022-04-27 22:45:28 +0800

    🚗update

--- beautiful-code/src/main/java/com/zgy/learn/beautifulcleancode/BeautifulCodeApplication.java
+++ beautiful-code/src/main/java/com/zgy/learn/beautifulcleancode/BeautifulCodeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeautifulCleanCodeApplication {
+public class BeautifulCodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeautifulCleanCodeApplication.class, args);
+        SpringApplication.run(BeautifulCodeApplication.class, args);
 
     }
 
