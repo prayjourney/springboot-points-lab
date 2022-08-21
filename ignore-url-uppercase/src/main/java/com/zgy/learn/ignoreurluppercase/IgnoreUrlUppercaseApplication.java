83a33b46b0679e7e1c893558d077af22f1a6b437
prayjourney.io <2247359268@qq.com>
2022-08-21 22:18:42 +0800

    🔪忽略URL大小写

---
+++ ignore-url-uppercase/src/main/java/com/zgy/learn/ignoreurluppercase/IgnoreUrlUppercaseApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.ignoreurluppercase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnoreUrlUppercaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgnoreUrlUppercaseApplication.class, args);
+    }
+
+}
