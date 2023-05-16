fcb76ad5e7dceeb3994fbc18c9830caeed806f07
prayjourney <2247359268@qq.com>
2023-05-16 09:44:32 +0800

    初始化

---
+++ jwt-solution/security-jwt-token/src/main/java/com/zgy/learn/securityjwttoken/SecurityJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.securityjwttoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtTokenApplication.class, args);
+	}
+
+}
---
+++ jwt-solution/security-jwt-token/src/test/java/com/zgy/learn/securityjwttoken/SecurityJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.securityjwttoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
