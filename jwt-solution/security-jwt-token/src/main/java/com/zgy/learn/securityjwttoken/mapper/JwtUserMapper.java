25369be154fb698a2369b63c2963477e0ecbcb89
prayjourney <2247359268@qq.com>
2023-05-17 14:28:37 +0800

    update

--- jwt-solution/security-jwt-token/src/main/java/com/zgy/learn/securityjwttoken/mapper/JwtUserMapper.java
+++ jwt-solution/security-jwt-token/src/main/java/com/zgy/learn/securityjwttoken/mapper/JwtUserMapper.java
@@ -1,5 +1,6 @@
 package com.zgy.learn.securityjwttoken.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zgy.learn.securityjwttoken.pojo.JwtUser;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,61 +10,34 @@ import java.util.List;
 
 @Mapper
 @Repository
-public interface JwtUserMapper {
+public interface JwtUserMapper extends BaseMapper<JwtUser> {
     /**
-     * 通过ID查询单条数据
-     *
-     * @param id 主键
-     * @return 实例对象
+     * 通过ID查询
      */
     JwtUser queryById(Integer id);
 
     /**
      * 统计总行数
-     *
-     * @param jwtUser 查询条件
-     * @return 总行数
      */
     long count(JwtUser jwtUser);
 
     /**
-     * 新增数据
-     *
-     * @param jwtUser 实例对象
-     * @return 影响行数
-     */
-    int insert(JwtUser jwtUser);
-
-    /**
-     * 批量新增数据（MyBatis原生foreach方法）
-     *
-     * @param entities List<JwtUser> 实例对象列表
-     * @return 影响行数
+     * 批量新增数据
      */
     int insertBatch(@Param("entities") List<JwtUser> entities);
 
     /**
-     * 批量新增或按主键更新数据（MyBatis原生foreach方法）
-     *
-     * @param entities List<JwtUser> 实例对象列表
-     * @return 影响行数
-     * @throws org.springframework.jdbc.BadSqlGrammarException 入参是空List的时候会抛SQL语句错误的异常，请自行校验入参
+     * 批量新增或按主键更新数据
      */
     int insertOrUpdateBatch(@Param("entities") List<JwtUser> entities);
 
     /**
      * 修改数据
-     *
-     * @param jwtUser 实例对象
-     * @return 影响行数
      */
     int update(JwtUser jwtUser);
 
     /**
      * 通过主键删除数据
-     *
-     * @param id 主键
-     * @return 影响行数
      */
     int deleteById(Integer id);
 
