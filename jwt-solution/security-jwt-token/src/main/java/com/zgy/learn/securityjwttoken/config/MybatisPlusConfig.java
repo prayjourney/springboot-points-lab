87732913a77cf89762d471e7834f114c74e21ad5
prayjourney <2247359268@qq.com>
2023-05-17 13:52:17 +0800

    mp config

---
+++ jwt-solution/security-jwt-token/src/main/java/com/zgy/learn/securityjwttoken/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.zgy.learn.securityjwttoken.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@MapperScan("com.zgy.learn.securityjwttoken.mapper")
+@Configuration
+public class MybatisPlusConfig {
+    // 分页插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+}
