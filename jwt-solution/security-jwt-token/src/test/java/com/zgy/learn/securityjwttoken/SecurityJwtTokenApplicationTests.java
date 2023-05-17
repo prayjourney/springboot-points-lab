0b744cb0ad75a2cfec87bc6923eea8390c662ff4
prayjourney <2247359268@qq.com>
2023-05-17 15:28:58 +0800

    生成用户

--- jwt-solution/security-jwt-token/src/test/java/com/zgy/learn/securityjwttoken/SecurityJwtTokenApplicationTests.java
+++ jwt-solution/security-jwt-token/src/test/java/com/zgy/learn/securityjwttoken/SecurityJwtTokenApplicationTests.java
@@ -5,6 +5,10 @@ import com.zgy.learn.securityjwttoken.pojo.JwtUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Date;
 
 @SpringBootTest
 class SecurityJwtTokenApplicationTests {
@@ -19,6 +23,46 @@ class SecurityJwtTokenApplicationTests {
     public void testMapper() {
         long count = jwtUserMapper.count(new JwtUser());
         System.out.println(count);
+    }
+
+    @Test
+    public void testInsertUser01() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        JwtUser jwtUser = new JwtUser();
+        String pwd = passwordEncoder.encode("123456");
+        jwtUser.setUsername("admin").setEnabled(1).setFirstLogin(0).setAuthorities("default").setCreateTime(new Date())
+                .setUpdateTime(new Date()).setPassword(pwd);
+        jwtUserMapper.insert(jwtUser);
+    }
+
+    @Test
+    public void testInsertUser02() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        JwtUser jwtUser = new JwtUser();
+        String pwd = passwordEncoder.encode("123456");
+        jwtUser.setUsername("zhangsan").setEnabled(1).setFirstLogin(0).setAuthorities("default").setCreateTime(new Date())
+                .setUpdateTime(new Date()).setPassword(pwd);
+        jwtUserMapper.insert(jwtUser);
+    }
+
+    @Test
+    public void testInsertUser03() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        JwtUser jwtUser = new JwtUser();
+        String pwd = passwordEncoder.encode("123456");
+        jwtUser.setUsername("root").setEnabled(1).setFirstLogin(0).setAuthorities("default").setCreateTime(new Date())
+                .setUpdateTime(new Date()).setPassword(pwd);
+        jwtUserMapper.insert(jwtUser);
+    }
+
+    @Test
+    public void testInsertUser04() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        JwtUser jwtUser = new JwtUser();
+        String pwd = passwordEncoder.encode("123456");
+        jwtUser.setUsername("xx").setEnabled(1).setFirstLogin(0).setAuthorities("").setCreateTime(new Date())
+                .setUpdateTime(new Date()).setPassword(pwd);
+        jwtUserMapper.insert(jwtUser);
     }
 
 }
