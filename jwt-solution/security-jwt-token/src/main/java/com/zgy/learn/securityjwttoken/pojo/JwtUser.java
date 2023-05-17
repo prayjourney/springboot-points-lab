9e3167af626ecb7dbdbfbc6393e04207acbc0205
prayjourney <2247359268@qq.com>
2023-05-17 15:28:39 +0800

    update

--- jwt-solution/security-jwt-token/src/main/java/com/zgy/learn/securityjwttoken/pojo/JwtUser.java
+++ jwt-solution/security-jwt-token/src/main/java/com/zgy/learn/securityjwttoken/pojo/JwtUser.java
@@ -1,16 +1,21 @@
 package com.zgy.learn.securityjwttoken.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 import java.io.Serializable;
 
 @Data
+@Accessors(chain = true)
 public class JwtUser implements Serializable {
     private static final long serialVersionUID = 1L;
     /**
      * 主键
      */
+    @TableId(type = IdType.AUTO)
     private Integer id;
     /**
      * 用户名
