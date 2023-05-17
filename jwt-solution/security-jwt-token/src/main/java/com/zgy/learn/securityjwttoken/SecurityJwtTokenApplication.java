62d9f984d6139e3841db7874654a9f77514272bc
prayjourney <2247359268@qq.com>
2023-05-17 13:49:51 +0800

    配置数据库

--- jwt-solution/security-jwt-token/src/main/java/com/zgy/learn/securityjwttoken/SecurityJwtTokenApplication.java
+++ jwt-solution/security-jwt-token/src/main/java/com/zgy/learn/securityjwttoken/SecurityJwtTokenApplication.java
@@ -3,11 +3,14 @@ package com.zgy.learn.securityjwttoken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 使用spring security + jjwt + mybatis plus完成前后分离的认证
+ */
 @SpringBootApplication
 public class SecurityJwtTokenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityJwtTokenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJwtTokenApplication.class, args);
+    }
 
 }
--- jwt-solution/security-jwt-token/src/test/java/com/zgy/learn/securityjwttoken/SecurityJwtTokenApplicationTests.java
+++ jwt-solution/security-jwt-token/src/test/java/com/zgy/learn/securityjwttoken/SecurityJwtTokenApplicationTests.java
@@ -1,13 +1,24 @@
 package com.zgy.learn.securityjwttoken;
 
+import com.zgy.learn.securityjwttoken.mapper.JwtUserMapper;
+import com.zgy.learn.securityjwttoken.pojo.JwtUser;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityJwtTokenApplicationTests {
+    @Autowired
+    private JwtUserMapper jwtUserMapper;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testMapper() {
+        long count = jwtUserMapper.count(new JwtUser());
+        System.out.println(count);
+    }
 
 }
