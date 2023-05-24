0945b4584403dcd99be0073566eab46a12fddd7b
z.g.y <2247359268@qq.com>
2023-05-24 10:57:29 +0800

    账号是否可用的测试

--- jwt-solution/security-jwt/src/test/java/com/zgy/learn/springsecurity/SpringSecurityLearnApplicationTests.java
+++ jwt-solution/security-jwt/src/test/java/com/zgy/learn/springsecurity/SpringSecurityLearnApplicationTests.java
@@ -1,10 +1,12 @@
 package com.zgy.learn.springsecurity;
 
 import com.zgy.learn.springsecurity.pojo.SecurityUser;
+import com.zgy.learn.springsecurity.service.MyAuthenticationService;
 import com.zgy.learn.springsecurity.service.SecurityUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Date;
@@ -12,7 +14,9 @@ import java.util.Date;
 @SpringBootTest
 class SpringSecurityLearnApplicationTests {
     @Autowired
-    SecurityUserService securityUserService;
+    private SecurityUserService securityUserService;
+    @Autowired
+    private MyAuthenticationService myAuthenticationService;
 
     @Test
     void contextLoads() {
@@ -79,6 +83,68 @@ class SpringSecurityLearnApplicationTests {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         boolean res = encoder.matches("123456", pwdInDB);
         System.out.println(res);
+    }
+
+    @Test
+    public void createCommonUser() {
+        String username = "zgy";
+        String password = "123456";
+        String roles = "ROLE_admin";
+        String authorities = "admin,user,insert,delete,update,select";
+
+        // 密码加密
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encryptPassword = encoder.encode(password);
+        Date now = new Date();
+        SecurityUser securityUser =
+                new SecurityUser().setUsername(username).setPassword(encryptPassword)
+                        .setRoles(roles).setAuthorities(authorities).setCreateTime(now).setUpdateTime(now);
+        securityUserService.insertUser(securityUser);
+    }
+
+    @Test
+    public void createCommonUser02() {
+        String username = "lisi";
+        String password = "123456";
+        String roles = "ROLE_admin";
+        String authorities = "admin,user,insert,delete,update,select";
+
+        // 密码加密
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encryptPassword = encoder.encode(password);
+        Date now = new Date();
+        SecurityUser securityUser =
+                new SecurityUser().setUsername(username).setPassword(encryptPassword)
+                        .setRoles(roles).setAuthorities(authorities).setCreateTime(now).setUpdateTime(now)
+                        .setEnabled(true).setAccountNonLocked(true).setAccountNonExpired(true).setCredentialsNonExpired(true);
+        securityUserService.insertUser(securityUser);
+    }
+
+    @Test
+    public void testMyAuthenticationService() {
+        UserDetails zhangsan = myAuthenticationService.loadUserByUsername("zhangsan");
+        System.out.println("账号可用: " + zhangsan.isEnabled());
+        System.out.println("账号未过期: " + zhangsan.isAccountNonExpired());
+        System.out.println("账号未锁定: " + zhangsan.isAccountNonLocked());
+        System.out.println("密码未过期: " + zhangsan.isCredentialsNonExpired());
+    }
+
+    @Test
+    public void testMyAuthenticationService02() {
+        UserDetails zgy = myAuthenticationService.loadUserByUsername("zgy");
+        System.out.println("账号可用: " + zgy.isEnabled());
+        System.out.println("账号未过期: " + zgy.isAccountNonExpired());
+        System.out.println("账号未锁定: " + zgy.isAccountNonLocked());
+        System.out.println("密码未过期: " + zgy.isCredentialsNonExpired());
+    }
+
+    @Test
+    public void testMyAuthenticationService03() {
+        UserDetails lisi = myAuthenticationService.loadUserByUsername("lisi");
+        System.out.println("账号可用: " + lisi.isEnabled());
+        System.out.println("账号未过期: " + lisi.isAccountNonExpired());
+        System.out.println("账号未锁定: " + lisi.isAccountNonLocked());
+        System.out.println("密码未过期: " + lisi.isCredentialsNonExpired());
     }
 
 }
