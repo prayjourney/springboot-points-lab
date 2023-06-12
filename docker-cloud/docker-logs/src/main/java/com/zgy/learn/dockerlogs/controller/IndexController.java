863c32229495aa2c0260795f1625f9f24c209265
prayjourney <2247359268@qq.com>
2023-06-13 00:08:48 +0800

    🎱开启定时任务

---
+++ docker-cloud/docker-logs/src/main/java/com/zgy/learn/dockerlogs/controller/IndexController.java
@@ -0,0 +1,42 @@
+package com.zgy.learn.dockerlogs.controller;
+
+import com.zgy.learn.dockerlogs.service.IndexService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class IndexController {
+    @Autowired
+    private IndexService indexService;
+
+    @GetMapping("")
+    public String index() {
+        return "index page";
+    }
+
+    @GetMapping("/date")
+    public String getDate(String userId) {
+        if (userId.equals("zhangsan")) {
+            log.error("forbidden user: {}", userId);
+            return "forbidden user";
+        } else if (userId.equals("admin")) {
+            log.error("warning user: {}", userId);
+            return "warning user";
+        }
+        return indexService.date(userId);
+    }
+
+    @GetMapping("/cacl")
+    public Integer cacl(Integer num) {
+        try {
+            return 100 / num;
+        } catch (Exception e) {
+            log.debug("error: {}", e.getMessage());
+            return -1;
+        }
+    }
+
+}
---
+++ docker-cloud/docker-logs/src/main/java/com/zgy/learn/dockerlogs/service/IndexService.java
@@ -0,0 +1,14 @@
+package com.zgy.learn.dockerlogs.service;
+
+import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class IndexService {
+    public String date(String userId) {
+        log.info("用户:{}, 获取时间, {}", userId, DateUtil.now());
+        return DateUtil.now();
+    }
+}
