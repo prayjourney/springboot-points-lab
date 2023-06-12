bc020fca02425b6ae7c61613c3b145c22478e024
prayjourney <2247359268@qq.com>
2023-06-13 00:06:43 +0800

    🍭init

---
+++ docker-cloud/docker-logs/src/test/java/com/zgy/learn/dockerlogs/DockerLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.dockerlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerLogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
