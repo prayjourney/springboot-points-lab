af4c18a3e99a79d648e67190d8b4e5771d74961f
prayjourney <2247359268@qq.com>
2024-03-17 17:35:05 +0800

    boot + minio init
    boot + minio init

---
+++ boot-minio/src/main/java/com/zgy/learn/minio/MinIOApplication.java
@@ -0,0 +1,14 @@
+package com.zgy.learn.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class MinIOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinIOApplication.class, args);
+    }
+
+}
---
+++ boot-minio/src/test/java/com/zgy/learn/minio/MinIOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MinIOApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
