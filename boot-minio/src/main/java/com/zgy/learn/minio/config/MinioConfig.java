08ed60d9b522724460064b7c3fae1fbcfb57ffa4
prayjourney <2247359268@qq.com>
2024-03-18 00:01:41 +0800

    依赖和配置
    依赖和配置

---
+++ boot-minio/src/main/java/com/zgy/learn/minio/config/MinioConfig.java
@@ -0,0 +1,22 @@
+package com.zgy.learn.minio.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+    @Value("${minio.endpoint}")
+    private String endpoint;
+    @Value("${minio.accessKey}")
+    private String accessKey;
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient getMinioClient() {
+        return MinioClient.builder().endpoint(endpoint).credentials(accessKey, secretKey).build();
+    }
+
+}
