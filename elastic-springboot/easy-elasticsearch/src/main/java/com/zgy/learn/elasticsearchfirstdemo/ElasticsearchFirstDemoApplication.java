9382a1b0b2bea5ecc0aae4bc85b48248262fc70d
prayjourney <2247359268@qq.com>
2024-03-29 01:25:35 +0800

    🥜easy elasticsearch

---
+++ elastic-springboot/easy-elasticsearch/src/main/java/com/zgy/learn/elasticsearchfirstdemo/ElasticsearchFirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.elasticsearchfirstdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchFirstDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchFirstDemoApplication.class, args);
+    }
+
+}
